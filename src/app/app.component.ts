131b3b83b9062f5fc6e126279adff19e3994d437
Paras Panchal <paraspanchal5555@gmail.com>
2024-01-18 22:02:46 +0530

    Commit for ngx-infinite-scroll in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngx-infintescrollproject1';
+  // onScroll() {
+  //   console.log("scrolled!!");
+  // }
+
+  ngOnInit(){
+    for(let i =1; i<=100;i++){
+      this.post.push(i);
+    }
+  }
+
+  post :any = [];  //1--100
+  onScroll(){
+    const length = this.post.length;   //[100].map
+    setTimeout(()=>{        //[1,2,3]
+      const p:any= ' '.repeat(100).split('').map((s,i)=>i+1 + length);
+      while(p.length) this.post.push(p.shift());
+    },1000);
+  }
 }
